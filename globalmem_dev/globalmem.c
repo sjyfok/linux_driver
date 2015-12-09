418849e5adfb550b63d6e1b254ae043a243618bd
song <song@song-VirtualBox.(none)>
2015-12-09 17:34:28 +0800

    globalmem_dev is frame is done

--- globalmem_dev/globalmem.c
+++ globalmem_dev/globalmem.c
@@ -1,8 +1,16 @@
 #include <linux/init.h>
 #include <linux/module.h>
-#include <linux/kernel.h>
+#include <linux/types.h>
+#include <linux/fs.h>
+#include <linux/errno.h>
+#include <linux/mm.h>
+#include <linux/sched.h>
+#include <linux/init.h>
 #include <linux/cdev.h>
-
+#include <linux/kdev_t.h>
+#include <asm/io.h>
+#include <asm/system.h>
+#include <asm/uaccess.h>
 
 
 #define	GLOBALMEM_SIZE   	0x1000
@@ -19,6 +27,134 @@ struct globalmem_dev
 
 struct globalmem_dev dev;
 
+static ssize_t globalmem_read(struct file *flip, char __user *buf, size_t count, loff_t *ppos)
+{
+	unsigned long p = *ppos;
+	
+	int ret = 0;
+	
+	if (p >= GLOBALMEM_SIZE)
+	{
+		return 0;
+	}
+	if (count > GLOBALMEM_SIZE - p)
+	{
+		count = GLOBALMEM_SIZE - p;
+	}
+	if (copy_to_user(buf, (void*)(dev.mem + p), count))
+	{
+		ret = -EFAULT;
+	}
+	else
+	{
+		*ppos = count;
+		ret = count;
+		printk(KERN_INFO "read %d bytes form %d\n", count, p);
+	}
+	return ret;
+}
+
+static ssize_t globalmem_write(struct file *filp, const char __user *buf, size_t count, loff_t *ppos)
+{
+	unsigned long p = *ppos;
+	int ret = 0;
+
+	if (p >= GLOBALMEM_SIZE)
+	{
+		return 0;
+	}
+	if (count > GLOBALMEM_SIZE - p)
+	{
+		count = GLOBALMEM_SIZE - p;
+	}
+	
+	if (copy_from_user(dev.mem + p, buf, count))
+	{
+		ret = -EFAULT;
+	}
+	else 
+	{
+		*ppos += count;
+		ret = count;
+		printk(KERN_INFO "written %d bytes form %d\n", count, p);
+	}
+	return ret;
+}
+
+static loff_t globalmem_llseek(struct file *filp, loff_t offset, int orig)
+{	
+	loff_t ret; 
+	switch (orig)
+	{
+		case 0:
+			if (offset < 0)
+			{
+				ret = -EINVAL;
+				break;
+			}
+			if ((unsigned int)offset > GLOBALMEM_SIZE)
+			{
+				ret = -EINVAL;
+				break;
+			}
+			filp->f_pos = (unsigned int)offset;
+			ret = filp->f_pos;
+			break;
+		case 1:
+			if ((filp->f_pos + offset) > GLOBALMEM_SIZE)
+			{
+				ret = -EINVAL;
+				break;
+			}
+			if ((filp->f_pos + offset) < 0)
+			{
+				ret = -EINVAL;
+				break;	
+			}
+			filp->f_pos += offset;
+			ret = filp->f_pos;
+			break;
+		default:
+			ret = -EINVAL;
+	}
+	return ret;
+}
+
+static int globalmem_ioctl(struct inode *inodep, struct file *filp, unsigned int cmd, unsigned long arg)
+{
+	switch (cmd)
+	{
+		case MEM_CLEAR:
+			memset(dev.mem, 0, GLOBALMEM_SIZE);
+			printk(KERN_INFO "globalmem is set to zero\n");
+			break;
+		default:
+			return -EINVAL;
+	}
+	return 0;
+}
+
+static const struct file_operations globalmem_fops = {
+	.owner 						= THIS_MODULE,
+	.llseek	 					= globalmem_llseek,
+	.read		 					= globalmem_read,
+	.write	 					= globalmem_write,
+	.unlocked_ioctl		= globalmem_ioctl,
+};
+
+static void globalmem_setup_cdev()
+{
+	int err, devno = MKDEV(globalmem_major, 0);
+	
+	cdev_init(&dev.cdev, &globalmem_fops);
+	dev.cdev.owner = THIS_MODULE;
+	err = cdev_add(&dev.cdev, devno, 1);
+	if (err)
+	{
+		printk(KERN_NOTICE "Error %d adding globalmem", err);
+	}
+}
+
 
 static int globalmem_init(void)
 {
@@ -32,8 +168,8 @@ static int globalmem_init(void)
 	}
 	else
 	{
-		result = allov_chrdev_region(devno, 0, 1, "globalmem");
-		globalmem_major = MKDEV(devno);
+		result = alloc_chrdev_region(&devno, 0, 1, "globalmem");
+		globalmem_major = MAJOR(devno);
 	}
 	if (result < 0)
 	{
@@ -49,6 +185,7 @@ static void globalmem_exit(void)
 	unregister_chrdev_region(MKDEV(globalmem_major, 0), 1);
   printk("globalmem leave see you\n");
 }
+
 
 module_init(globalmem_init);
 module_exit(globalmem_exit);
